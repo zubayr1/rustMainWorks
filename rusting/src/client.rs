0f7d175660ac9810c970c46810e2dd82f022b99b
zubayr1 <write2zubayr@gmail.com>
2023-05-05 20:13:48 +0200

    simplify client

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -49,11 +49,7 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
         let three_millis = time::Duration::from_millis(3);
         thread::sleep(three_millis);
     }
-
-    if TcpStream::connect(addressclone.clone()).await.is_err()
-    {
-        println!("not");
-    }
+   
 
    // if TcpStream::connect(addressclone.clone()).await.is_ok(){
 
@@ -68,33 +64,33 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
     
     println!("aaa{}", addressclone);
 
-
-    if types == "none" // types == "none": first time communication
-    {   
-        if behavior=="1" // if 1: can act as adversary and send false signature
-        {
-            let false_key = schnorrkel::_create_adversarial_key();
-            println!("bbb");
-            write.write_all(false_key.as_bytes()).await.unwrap();
+    write.write_all(b"EOF").await.unwrap();
+    // if types == "none" // types == "none": first time communication
+    // {   
+    //     if behavior=="1" // if 1: can act as adversary and send false signature
+    //     {
+    //         let false_key = schnorrkel::_create_adversarial_key();
+    //         println!("bbb");
+    //         write.write_all(false_key.as_bytes()).await.unwrap();
             
-        }
-        else // acts as honest node
-        {
-            write.write_all(pubkey.as_bytes()).await.unwrap();
-        }
+    //     }
+    //     else // acts as honest node
+    //     {
+    //         write.write_all(pubkey.as_bytes()).await.unwrap();
+    //     }
         
-        write.write_all(sign.as_bytes()).await.unwrap(); // write signature to server.
-        let id = [self_ip.to_string(), "messageEOF".to_string()].join(" ");
-        write.write_all(id.as_bytes()).await.unwrap();
+    //     write.write_all(sign.as_bytes()).await.unwrap(); // write signature to server.
+    //     let id = [self_ip.to_string(), "messageEOF".to_string()].join(" ");
+    //     write.write_all(id.as_bytes()).await.unwrap();
 
-        println!("ccc");
-    } 
-    else // next communication. REACTOR to be used here
-    {
-        write.write_all(types.as_bytes()).await.unwrap();
-        write.write_all(types.as_bytes()).await.unwrap();
-        write.write_all(b"EOF").await.unwrap();
-    }
+    //     println!("ccc");
+    // } 
+    // else // next communication. REACTOR to be used here
+    // {
+    //     write.write_all(types.as_bytes()).await.unwrap();
+    //     write.write_all(types.as_bytes()).await.unwrap();
+    //     write.write_all(b"EOF").await.unwrap();
+    // }
 
 //}
         
