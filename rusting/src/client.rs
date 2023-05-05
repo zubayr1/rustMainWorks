633b1712ccb9ad3c9eb02c3815743aead067ac61
zubayr1 <write2zubayr@gmail.com>
2023-05-05 20:11:33 +0200

    kuttar bacca

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -50,7 +50,10 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
         thread::sleep(three_millis);
     }
 
-    
+    if TcpStream::connect(addressclone.clone()).await.is_err()
+    {
+        println!("not");
+    }
 
    // if TcpStream::connect(addressclone.clone()).await.is_ok(){
 
