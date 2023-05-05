af501fe7247b6141c5fc60aa39ef3299886b54d2
zubayr1 <write2zubayr@gmail.com>
2023-05-06 00:57:50 +0200

    result check

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -77,6 +77,7 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
         
     }
     let _result = write.write_all(b"EOF").await.unwrap();
+    println!("{:?}", _result);
     Ok(())
 
 }
