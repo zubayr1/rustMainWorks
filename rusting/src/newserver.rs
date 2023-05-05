bf43b5263d3d3e2488f0e186d2174ad6e80e68cf
zubayr1 <write2zubayr@gmail.com>
2023-05-06 01:21:16 +0200

    newserver

--- rusting/src/newserver.rs
+++ rusting/src/newserver.rs
@@ -7,6 +7,9 @@ use std::collections::HashSet;
 pub async fn handle_server(server_type: String, ip_address: Vec<String>, args: Vec<String>, self_ip: String, port: u32, epoch: i32, mut blacklisted: HashSet<String>) -> Result<(), Box<dyn Error>> {
     let mut data = [0u8; 12];
     println!("server");
+    let mut count=0;
+    loop{
+        count+=1;
     let listener = TcpListener::bind(["0.0.0.0".to_string(), port.to_string()].join(":")).await?;
     let (tokio_tcp_stream, _) = listener.accept().await?;
     println!("server done");
@@ -14,5 +17,11 @@ pub async fn handle_server(server_type: String, ip_address: Vec<String>, args: V
     std_tcp_stream.set_nonblocking(false)?;
     std_tcp_stream.read_exact(&mut data)?;
     println!("{:?}", data);
+
+    if count==4
+    {
+        break;
+    }
+    }
     Ok(())
 }
