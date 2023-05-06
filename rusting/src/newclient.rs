98adf4e36335802ae9ee06ae27d3e36565b80046
zubayr1 <write2zubayr@gmail.com>
2023-05-06 06:06:51 +0200

    newclient

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -6,9 +6,10 @@ use std::error::Error;
 pub async fn match_tcp_client(address: String, self_ip: String, types: String, epoch: i32, behavior: String) -> Result<(), Box<dyn Error>> {
     // Connect to a peer
     println!("client {}", address);
-    let mut stream = TcpStream::connect(address).await?;
+    let mut stream = TcpStream::connect(address.clone()).await?;
     println!("client done");
     // Write some data.
+    stream.write_all(address.as_bytes()).await?;
     stream.write_all(b"hello world!EOF").await?;
     // stream.shutdown().await?;
     Ok(())
