0a94769ef83a1d9464f5d2bf4a474380a905adba
zubayr1 <write2zubayr@gmail.com>
2023-05-06 05:28:04 +0200

    client

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -79,10 +79,10 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
     //     write.write_all(types.as_bytes()).await.unwrap();
         
     // }
-    // let _result = stream.write([self_ip.to_string(), "EOF".to_string()].join(" ").as_bytes()).await;
-    // // write.shutdown().await?;
+    let _result = stream.write([self_ip.to_string(), "EOF".to_string()].join(" ").as_bytes()).await;
+    // write.shutdown().await?;
     
-    // println!("{:?}", _result.is_ok());
+    println!("{:?}", _result.is_ok());
 
     while stream.write([self_ip.to_string(), "EOF".to_string()].join(" ").as_bytes()).await.is_err()
     {
