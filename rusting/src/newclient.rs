0f39af087f221789e05909ae07deded7f0b2daad
zubayr1 <write2zubayr@gmail.com>
2023-05-06 16:25:21 +0200

    newclient

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -34,7 +34,7 @@ pub async fn match_tcp_client(address: String, self_ip: String) -> Result<(), Bo
     stream.write_all(self_ip.as_bytes()).await?;
     stream.write_all(b"hello world!EOF").await?;
     
-    stream.shutdown().await?;
+   // stream.shutdown().await?;
    // break;
    // }
 
