bdee7bcdbc3ffac4abe5620fbd20d5bd1cf4e040
zubayr1 <write2zubayr@gmail.com>
2023-05-06 04:56:15 +0200

    client

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -52,30 +52,30 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
     
 
     
-    if types == "none" // types == "none": first time communication
-    {   
-        if behavior=="1" // if 1: can act as adversary and send false signature
-        {
-            let false_key = schnorrkel::_create_adversarial_key();
-            write.write_all(false_key.as_bytes()).await.unwrap();
+    // if types == "none" // types == "none": first time communication
+    // {   
+    //     if behavior=="1" // if 1: can act as adversary and send false signature
+    //     {
+    //         let false_key = schnorrkel::_create_adversarial_key();
+    //         write.write_all(false_key.as_bytes()).await.unwrap();
             
-        }
-        else // acts as honest node
-        {
-            write.write_all(pubkey.as_bytes()).await.unwrap();
-        }
+    //     }
+    //     else // acts as honest node
+    //     {
+    //         write.write_all(pubkey.as_bytes()).await.unwrap();
+    //     }
         
-        write.write_all(sign.as_bytes()).await.unwrap(); // write signature to server.
-        let id = [self_ip.to_string(), "message".to_string()].join(" ");
-        write.write_all(id.as_bytes()).await.unwrap();
+    //     write.write_all(sign.as_bytes()).await.unwrap(); // write signature to server.
+    //     let id = [self_ip.to_string(), "message".to_string()].join(" ");
+    //     write.write_all(id.as_bytes()).await.unwrap();
 
-    } 
-    else // next communication. REACTOR to be used here
-    {
-        write.write_all(types.as_bytes()).await.unwrap();
-        write.write_all(types.as_bytes()).await.unwrap();
+    // } 
+    // else // next communication. REACTOR to be used here
+    // {
+    //     write.write_all(types.as_bytes()).await.unwrap();
+    //     write.write_all(types.as_bytes()).await.unwrap();
         
-    }
+    // }
     let _result = write.write_all(b"EOF").await?;
     // write.shutdown().await?;
 
