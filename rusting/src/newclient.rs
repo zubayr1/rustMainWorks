845bdb090dde2a1d3e80d8d6dee83705b4b97ad1
zubayr1 <write2zubayr@gmail.com>
2023-05-06 16:37:03 +0200

    newclient

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -16,18 +16,10 @@ pub async fn match_tcp_client(address: String, self_ip: String) -> Result<(), Bo
 
    
     
-    stream.set_linger(None)?;
 
     println!("connected from {} to address {}", self_ip, address);
 
 
-    let sock_ref = socket2::SockRef::from(&stream);
-
-    let mut ka = socket2::TcpKeepalive::new();
-    ka = ka.with_time(time::Duration::from_secs(20));
-    ka = ka.with_interval(time::Duration::from_secs(20));
-
-    sock_ref.set_tcp_keepalive(&ka)?;
 
 
     // Write some data.
