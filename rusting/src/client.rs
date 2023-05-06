7d393d6ff0f1154a2fd55603111f37f71e14afa4
zubayr1 <write2zubayr@gmail.com>
2023-05-06 02:12:22 +0200

    loopback

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -77,7 +77,7 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
         
     }
     let _result = write.write_all(b"EOF").await.unwrap();
-    write.shutdown().await?;
+    // write.shutdown().await?;
 
     Ok(())
 
