0cca520fd7216580fa7d4834dbc5dc7db63ed298
zubayr1 <write2zubayr@gmail.com>
2023-05-06 12:45:05 +0200

    newclient

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -1,14 +1,27 @@
 use tokio::net::TcpStream;
 use tokio::io::AsyncWriteExt;
 use std::error::Error;
+use socket2::{Socket, Domain, Type};
+use std::{thread, time};
 
 #[tokio::main]
 pub async fn match_tcp_client(address: String, self_ip: String) -> Result<(), Box<dyn Error>> {
     // Connect to a peer
     let mut stream = TcpStream::connect(address.clone()).await?;
+
+    let sock_ref = socket2::SockRef::from(&stream);
+
+    let mut ka = socket2::TcpKeepalive::new();
+    ka = ka.with_time(time::Duration::from_secs(20));
+    ka = ka.with_interval(time::Duration::from_secs(20));
+
+    sock_ref.set_tcp_keepalive(&ka)?;
+
+
     // Write some data.
     stream.write_all(self_ip.as_bytes()).await?;
     stream.write_all(b"hello world!EOF").await?;
-    // stream.shutdown().await?;
+    
+    
     Ok(())
 }
