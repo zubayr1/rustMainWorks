4702961dbebf99244398b160d63e47d4d0f0c87a
zubayr1 <write2zubayr@gmail.com>
2023-05-07 10:52:59 +0200

    newserver

--- rusting/src/newserver.rs
+++ rusting/src/newserver.rs
@@ -4,7 +4,7 @@ use tokio::net::tcp::ReadHalf;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 
 #[tokio::main]
-pub async fn handle_server( port: u32) -> Result<(), Box<dyn Error>>{
+pub async fn handle_server( ip_address: Vec<String>, port: u32) -> Result<(), Box<dyn Error>>{
 
    // loop{
     let listener = TcpListener::bind(["0.0.0.0".to_string(), port.to_string()].join(":")).await.unwrap(); // open connection
