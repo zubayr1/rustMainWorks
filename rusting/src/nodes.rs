e899c3c4355e6bce7d3b7a58ba1951bdac80bfc7
zubayr1 <write2zubayr@gmail.com>
2023-05-08 00:02:19 +0200

    nodes

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -124,8 +124,9 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                                 }
                             }
 
-                            if check_connect(ip_clone.clone()).is_err() && count>4
+                            if check_connect(ip_clone.clone()).is_err() && count>=4
                             {
+                                println!("bbb");
                                 break;
                             }
 
