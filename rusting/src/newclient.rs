119273eded2121a8934435ee6c151da6bc028fea
zubayr1 <write2zubayr@gmail.com>
2023-05-07 22:23:55 +0200

    new client

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -10,7 +10,7 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 #[tokio::main]
 pub async fn match_tcp_client(address: String, self_ip: String) -> Result<(), Box<dyn Error>> {
     // Connect to a peer
-    println!("trying to connect from {} to address {}", self_ip, address);
+   // println!("trying to connect from {} to address {}", self_ip, address);
 
     // while TcpStream::connect(address.clone()).await.is_err() //waiting for server to be active, if not random wait and retry
     // {
@@ -53,7 +53,7 @@ pub async fn match_tcp_client(address: String, self_ip: String) -> Result<(), Bo
     let mut stream: TcpStream = TcpStream::connect(address.clone()).await?;
     // stream.set_linger(Some(Duration::from_secs(10))).expect("set_linger call failed");
 
-   println!("connected from {} to address {}", self_ip, address);
+  // println!("connected from {} to address {}", self_ip, address);
 
 
   loop{
