8553cb4c6cc967f71985dbab8477e53aa63ce438
zubayr1 <write2zubayr@gmail.com>
2023-05-08 01:59:28 +0200

    nodes

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -92,9 +92,10 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                         {
                             
                             let _result = newserver::handle_server( ip_address_clone.clone(), INITIAL_PORT+port_count  );
-                            
+                            println!("{}", _result);
                             if _result!="NA"
                             {
+                                
                                 retry_ips.push(_result.clone());
                             }
                         }
