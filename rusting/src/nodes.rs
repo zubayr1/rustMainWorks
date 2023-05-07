449faca0e34a9c35ce6266c8750affec4235486b
zubayr1 <write2zubayr@gmail.com>
2023-05-08 00:16:35 +0200

    nodes

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -111,8 +111,8 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                         for ip in ip_address_clone.clone() 
                         {
                             let ip_clone = ip.clone();
-                            // if !accepted_ips.contains(&ip.clone())
-                            // {
+                            if !accepted_ips.contains(&ip.clone())
+                            {
                                 let self_ip_clone = self_ip.clone();
         
                                 
@@ -124,7 +124,7 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                                     accepted_ips.push(ip);
                                     count+=1;
                                 }
-                           // }
+                            }
 
                             if  count>=400
                             {
