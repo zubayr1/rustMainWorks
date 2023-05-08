6e9f7062da742a6c584106d51b35844bb7f331db
zubayr1 <write2zubayr@gmail.com>
2023-05-08 13:41:52 +0200

    new server

--- rusting/src/newserver.rs
+++ rusting/src/newserver.rs
@@ -31,10 +31,10 @@ pub async fn handle_server( ip_address: Vec<String>, port: u32, testport: u32) -
      
         let _bytes_read: usize = reader.read_line(&mut line).await.unwrap();
 
-    //    if _bytes_read==0
-    //    {       
-    //     break;
-    //    }
+       if _bytes_read==0
+       {       
+        break;
+       }
         
         if line.contains("EOF")  //REACTOR to be used here
         {
