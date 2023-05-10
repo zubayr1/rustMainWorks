97f080f27741a3ccdc27790b476b8486d729c659
zubayr1 <write2zubayr@gmail.com>
2023-05-11 01:58:15 +0200

    nodes

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -98,7 +98,7 @@ pub async fn initiate(filtered_committee: HashMap<u32, String>, args: Vec<String
                             
                             
                             let _result = newserver::handle_server( ip_address_clone.clone(), INITIAL_PORT+port_count, TEST_PORT+port_count + additional_port );
-
+                            println!("{}", _result);
                         }
                         
                        
