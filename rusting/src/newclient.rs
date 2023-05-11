0d0d8640f95f5add18da6b8417b57f33d1d3cb3a
zubayr1 <write2zubayr@gmail.com>
2023-05-11 02:04:44 +0200

    newclient

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -11,7 +11,7 @@ pub async fn match_tcp_client(address: String, test_address: String, self_ip: St
     let mut file = OpenOptions::new().append(true).open("output.log").await.unwrap();
 
     // Connect to a peer
-
+    println!("adress{}", address);
     while TcpStream::connect(test_address.clone()).await.is_err() //waiting for server to be active, if not random wait and retry
     {
         sleep(Duration::from_millis(3)).await;
