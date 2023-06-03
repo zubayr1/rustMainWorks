6950d37d13eff3d5754770f12bdf97c2d614f970
zubayr1 <write2zubayr@gmail.com>
2023-06-03 14:02:54 +0200

    update run setup for general ip, id

--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
+IP=$(curl checkip.amazonaws.com)
+
+ID="null"
+
+# Collect IPs
+IPS=()
+FILE="${1:-nodes_information.txt}"
+
+# Read ip file
+while IFS= read -r line; do
+    IPS+=($line)
+done < $FILE
+
+
+# Connect to every IP and run setup.sh on instance
+for _ip in "${IPS[@]}"
+do
+    # Format of ip file NEEDS to be id-ip
+    tmp=(${_ip//-/ })
+    ip=${tmp[1]}
+
+    if [ "$ip" = "$IP" ]; then
+        ID=${tmp[0]}
+        fi
+done
+
+
 cd "rustMainWorks/rusting"
 
-/home/ubuntu/.cargo/bin/cargo run -- keys {1} 4 03282129 prod {18.117.92.19} 4 0
+/home/ubuntu/.cargo/bin/cargo run -- nok "$ID" 4 05050021  prod "$IP" 10 1
--- setup.sh
+++ setup.sh
@@ -1,3 +1,30 @@
+IP=$(curl checkip.amazonaws.com)
+
+ID="null"
+
+# Collect IPs
+IPS=()
+FILE="${1:-nodes_information.txt}"
+
+# Read ip file
+while IFS= read -r line; do
+    IPS+=($line)
+done < $FILE
+
+
+# Connect to every IP and run setup.sh on instance
+for _ip in "${IPS[@]}"
+do
+    # Format of ip file NEEDS to be id-ip
+    tmp=(${_ip//-/ })
+    ip=${tmp[1]}
+
+    if [ "$ip" = "$IP" ]; then
+        ID=${tmp[0]}
+        fi
+done
+
+
 folder="rustMainWorks"
 
 if [ -d "${folder}" ]; then
@@ -13,4 +40,5 @@ fi
 # run script
 cd "rusting"
 python3 Structuring.py
-/home/ubuntu/.cargo/bin/cargo run -- keys {1} 4 03282129 prod {18.117.92.19} 4 0
+/home/ubuntu/.cargo/bin/cargo run -- keys "$ID" 4 03282129 prod "$IP" 10 1
+exit 0
