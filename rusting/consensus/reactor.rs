2881206a279b2f8c40f3b43c3fc6bb4b88adb8d2
zubayr1 <write2zubayr@gmail.com>
2023-06-19 23:38:32 +0200

    update reactor

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1,8 +1,28 @@
+enum Phase 
+{
+    echo, vote, committee, codeword, accum
+}
+
+impl Phase 
+{
+    pub fn is_weekday(&self) -> bool
+    {
+        match self 
+        {
+            &Phase:: echo => return false,
+            _=> return true
+        }
+    }
+}
+
 
 pub async fn reactor_init(line: String) -> String
 {
+    // let d = Phase::line;
+
     if line.contains("echo")
     {
+        
         return "echo".to_string();
     }
     else if line.contains("vote")
