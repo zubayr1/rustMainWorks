9b80a4a695a50590f0c9b584ae8bc71c851b2f68
zubayr1 <write2zubayr@gmail.com>
2023-06-22 19:16:19 +0200

    add echo, vote impl

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1,3 +1,6 @@
+#[path = "../types/generic.rs"]
+mod generic; 
+
 enum Phase 
 {
     echo, vote, committee, codeword, accum
@@ -18,19 +21,20 @@ impl Phase
 
 pub async fn reactor_init(line: String) -> String
 {
-    // let d = Phase::line;
 
     if line.contains("echo")
     {
-        
+        let echo = generic::Echo::create_echo("".to_string(), "".to_string());
         return "echo".to_string();
     }
     else if line.contains("vote")
     {
+        let vote = generic::Vote::create_vote("".to_string(), "".to_string());
         return "vote".to_string();
     }
     if line.contains("committee")
     {
+        let committee = generic::Committee::create_committee("".to_string(), "".to_string());
         return "committee".to_string();
     }
     else if line.contains("codeword")
--- rusting/types/generic.rs
+++ rusting/types/generic.rs
@@ -1,13 +1,56 @@
-pub struct echo
+pub struct Echo
 {
     pub sign: String,
     pub value: String
 }
 
-impl echo
+impl Echo
 {
-    pub fn send_echo(&self)
+    pub fn create_echo(sign: String, value: String) -> Self
     {
-        
+        Echo{sign:sign, value: value}
+    }
+}
+
+pub struct Vote
+{
+    pub sign: String,
+    pub value: String
+}
+
+impl Vote
+{
+    pub fn create_vote(sign: String, value: String) -> Self
+    {
+        Vote{sign:sign, value: value}
+    }
+}
+
+
+pub struct Committee
+{
+    pub sign: String,
+    pub value: String
+}
+
+impl Committee
+{
+    pub fn create_committee(sign: String, value: String) -> Self
+    {
+        Committee{sign:sign, value: value}
+    }
+}
+
+pub struct Codeword
+{
+    pub sign: String,
+    pub value: String
+}
+
+impl Codeword
+{
+    pub fn create_codeword(sign: String, value: String) -> Self
+    {
+        Codeword{sign:sign, value: value}
     }
 }
