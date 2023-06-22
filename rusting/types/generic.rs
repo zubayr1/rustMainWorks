ebb5cb68bb042637631e5406e0220af86fd62003
zubayr1 <write2zubayr@gmail.com>
2023-06-22 21:38:19 +0200

    generic types of message

--- rusting/types/generic.rs
+++ rusting/types/generic.rs
@@ -1,28 +1,30 @@
 pub struct Echo
 {
     pub sign: String,
-    pub value: String
+    pub value: String,
+    pub types: String
 }
 
 impl Echo
 {
     pub fn create_echo(sign: String, value: String) -> Self
     {
-        Echo{sign:sign, value: value}
+        Echo{sign:sign, value: value, types: "echo".to_string()}
     }
 }
 
 pub struct Vote
 {
     pub sign: String,
-    pub value: String
+    pub value: String,
+    pub types: String
 }
 
 impl Vote
 {
     pub fn create_vote(sign: String, value: String) -> Self
     {
-        Vote{sign:sign, value: value}
+        Vote{sign:sign, value: value, types: "vote".to_string()}
     }
 }
 
@@ -30,14 +32,15 @@ impl Vote
 pub struct Committee
 {
     pub sign: String,
-    pub value: String
+    pub value: String,
+    pub types: String
 }
 
 impl Committee
 {
     pub fn create_committee(sign: String, value: String) -> Self
     {
-        Committee{sign:sign, value: value}
+        Committee{sign:sign, value: value, types: "committee".to_string()}
     }
 }
 
@@ -46,27 +49,29 @@ pub struct Codeword
     pub sign: String,
     pub codewords: String,
     pub witness: String,
-    pub accumulation_value: String
+    pub accumulation_value: String,
+    pub types: String
 }
 
 impl Codeword
 {
     pub fn create_codeword(sign: String, codewords: String, witness: String, accumulation_value: String) -> Self
     {
-        Codeword{sign:sign, codewords: codewords, witness: witness, accumulation_value: accumulation_value}
+        Codeword{sign:sign, codewords: codewords, witness: witness, accumulation_value: accumulation_value, types: "codeword".to_string()}
     }
 }
 
 pub struct Accum
 {
     pub sign: String,
-    pub accumulation_value: String
+    pub accumulation_value: String,
+    pub types: String
 }
 
 impl Accum
 {
     pub fn create_accum(sign: String, accumulation_value: String) -> Self
     {
-        Accum{sign:sign, accumulation_value: accumulation_value}
+        Accum{sign:sign, accumulation_value: accumulation_value, types: "accum".to_string()}
     }
 }
