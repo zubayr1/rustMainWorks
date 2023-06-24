7678686e4e5ae2e8fcdba3ae04d303fce30cc41d
zubayr1 <write2zubayr@gmail.com>
2023-06-24 11:54:35 +0200

    reactor

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1,5 +1,18 @@
+use std::{thread, time};
+use std::error::Error;
+use std::fs::OpenOptions;
+
+#[path = "../networking/newclient.rs"]
+mod newclient;
+
+#[path = "../networking/newserver.rs"]
+mod newserver;
+
 #[path = "../types/generic.rs"]
 mod generic; 
+
+const INITIAL_PORT: u32 = 7821;
+const TEST_PORT: u32 = 7921;
 
 enum Phase 
 {
@@ -16,6 +29,81 @@ impl Phase
             _=> return true
         }
     }
+}
+
+
+async fn prod_communication(sorted: Vec<(&u32, &String)>, mut port_count: u32, _index:u32, args: Vec<String>, message_type: String)
+{
+    let mut file: std::fs::File = OpenOptions::new().append(true).open("output.log").unwrap();
+
+    let mut level=0;
+
+    let mut text;
+
+    text = ["epoch ".to_string(), _index.to_string()].join(": ");
+    file.write_all(text.as_bytes()).unwrap();
+    file.write_all(b"\n").unwrap();
+
+    for (_i, ip_addresses_comb) in sorted.clone()
+    {
+        port_count+=1;
+
+        let ip_address: Vec<&str> = ip_addresses_comb.split(" ").collect();
+
+        let ip_address_clone = ip_address.clone();
+        
+        text = ["Level ".to_string(), level.to_string()].join(": ");
+        file.write_all(text.as_bytes()).unwrap();
+        file.write_all(b"\n").unwrap();
+        level+=1;
+                
+        thread::scope(|s| { 
+
+            s.spawn(|| 
+            {
+                
+                let mut count=1;
+                for _ip in ip_address_clone.clone() 
+                {
+                    count+=1;
+                    let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
+                    
+                    let _result = newserver::handle_server( ip_address_clone.clone(), INITIAL_PORT+port_count, TEST_PORT+port_count + additional_port );
+                }
+                
+                
+            });
+
+                            
+            s.spawn(|| {
+                let three_millis = time::Duration::from_millis(3);
+                thread::sleep(three_millis);
+
+                let mut count=1;
+
+                for ip in ip_address_clone.clone() 
+                {
+                    count+=1;
+                    let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
+
+                    let _result: Result<(), Box<dyn Error>> = newclient::match_tcp_client([ip.to_string(), (INITIAL_PORT+port_count).to_string()].join(":"),
+                    [ip.to_string(), (TEST_PORT+port_count + additional_port).to_string()].join(":"), message_type.clone());
+
+                    
+                }
+
+            });
+
+        });
+    }
+
+
+}
+
+
+async fn dev_communication(working_port: String, test_port: String, message_type: String)
+{
+    let _result: Result<(), Box<dyn Error>> = newclient::match_tcp_client(working_port, test_port, message_type);
 }
 
 
--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -1,7 +1,5 @@
 
 use std::io::Write;
-use std::{thread, time};
-use std::error::Error;
 use std::fs::OpenOptions;
 use std::collections::HashMap;
 use chrono::Utc;
@@ -34,79 +32,6 @@ pub fn create_keys() // schnorr key generation
 
 }
 
-
-async fn prod_communication(sorted: Vec<(&u32, &String)>, mut port_count: u32, _index:u32, args: Vec<String>, message_type: String)
-{
-    let mut file: std::fs::File = OpenOptions::new().append(true).open("output.log").unwrap();
-
-    let mut level=0;
-
-    let mut text;
-
-    text = ["epoch ".to_string(), _index.to_string()].join(": ");
-    file.write_all(text.as_bytes()).unwrap();
-    file.write_all(b"\n").unwrap();
-
-    for (_i, ip_addresses_comb) in sorted.clone()
-    {
-        port_count+=1;
-
-        let ip_address: Vec<&str> = ip_addresses_comb.split(" ").collect();
-
-        let ip_address_clone = ip_address.clone();
-        
-        text = ["Level ".to_string(), level.to_string()].join(": ");
-        file.write_all(text.as_bytes()).unwrap();
-        file.write_all(b"\n").unwrap();
-        level+=1;
-                
-        thread::scope(|s| { 
-
-            s.spawn(|| 
-            {
-                
-                let mut count=1;
-                for _ip in ip_address_clone.clone() 
-                {
-                    count+=1;
-                    let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
-                    
-                    let _result = newserver::handle_server( ip_address_clone.clone(), INITIAL_PORT+port_count, TEST_PORT+port_count + additional_port );
-                }
-                
-                
-            });
-
-                            
-            s.spawn(|| {
-                let three_millis = time::Duration::from_millis(3);
-                thread::sleep(three_millis);
-
-                let mut count=1;
-
-                for ip in ip_address_clone.clone() 
-                {
-                    count+=1;
-                    let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
-
-                    let _result: Result<(), Box<dyn Error>> = newclient::match_tcp_client([ip.to_string(), (INITIAL_PORT+port_count).to_string()].join(":"),
-                    [ip.to_string(), (TEST_PORT+port_count + additional_port).to_string()].join(":"), message_type.clone());
-
-                    
-                }
-
-            });
-
-        });
-    }
-
-
-}
-
-async fn dev_communication(working_port: String, test_port: String, message_type: String)
-{
-    let _result: Result<(), Box<dyn Error>> = newclient::match_tcp_client(working_port, test_port, message_type);
-}
 
 pub async fn initiate(filtered_committee: HashMap<u32, String>, args: Vec<String>)
 {  
