87b82751d2f54712177e046fcfe600b3695b1b3b
zubayr1 <write2zubayr@gmail.com>
2023-07-03 00:38:32 +0200

    check

--- rusting/types/accum.rs
+++ rusting/types/accum.rs
@@ -2,12 +2,16 @@ pub fn accum_reaction(received_texts: Vec<String>, committee_length: usize)
 {
     let mut C1: Vec<String> = Vec::new();
 
-    if (received_texts.len())>=committee_length/2
-    {
-        for text in received_texts
-        {
-            println!("{:?}", text);
-        }
+    // if (received_texts.len())>=committee_length/2
+    // {
+    //     for text in received_texts
+    //     {
+    //         let collection: Vec<&str> = text.split(", ").collect();
+    //         println!("{:?}", collection.len());
+    //         //C1.push(collection[1].to_string());
+    //     }
+
+    //     //println!("{:?}", C1);
         
-    }
+    // }
 }
