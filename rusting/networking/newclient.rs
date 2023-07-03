10465de3d42fa9d51a76baa0244e27b6f9c81452
zubayr1 <write2zubayr@gmail.com>
2023-07-03 17:47:03 +0200

    fix args[6]

--- rusting/networking/newclient.rs
+++ rusting/networking/newclient.rs
@@ -17,26 +17,19 @@ pub async fn match_tcp_client(address: String, test_address: String, value: Vec<
         sleep(Duration::from_millis(3)).await;
     }    
     let mut stream: TcpStream = TcpStream::connect(address.clone()).await?;
-    
-
-    let mut self_ip="0.0.0.0".to_string();
-
-    if let Ok(peer_addr) = stream.peer_addr() {
-        self_ip = peer_addr.ip().to_string();
-    } 
+     
 
     let value_string = value.iter().map(|n| n.to_string()).collect::<Vec<String>>().join(", ");
 
     loop
     {
         // Write data.
-        stream.write_all(self_ip.as_bytes()).await?;
         stream.write_all(args[6].to_string().as_bytes()).await?;
         
         let result = stream.write_all([value_string.clone(), "EOF".to_string().to_string()].join(" ").as_bytes()).await;
         if  result.is_ok()
         {
-            let text = ["client at: ".to_string(), self_ip.to_string()].join(": ");
+            let text = ["client at: ".to_string(), args[6].to_string()].join(": ");
             file.write_all(text.as_bytes()).await.unwrap();
             file.write_all(b"\n").await.unwrap();
             break;
