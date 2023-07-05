9cf91cbfe150ee0577fdf9eb8b9c94c86d950133
zubayr1 <write2zubayr@gmail.com>
2023-07-05 15:32:01 +0200

    check c value

--- rusting/types/accum.rs
+++ rusting/types/accum.rs
@@ -13,7 +13,7 @@ pub fn accum_check(received_texts: Vec<String>, committee_length: usize) -> bool
 
 pub fn accum_reaction(medium: String, received_texts: Vec<String>) -> Vec<(String, String)>
 {
-    let mut c1: Vec<(String, String)> = Vec::new();
+    let mut c: Vec<(String, String)> = Vec::new();
     
     if medium=="prod_init"
     {
@@ -21,27 +21,26 @@ pub fn accum_reaction(medium: String, received_texts: Vec<String>) -> Vec<(Strin
         {
         let split_text: Vec<&str> = text.split(',').collect();
         let accum_tuple = (split_text[0].to_string(), split_text[1].to_string());
-        c1.push(accum_tuple);
+        c.push(accum_tuple);
         }             
     }
     else 
     {
         let accum_tuple = (received_texts[0].to_string(), received_texts[1].to_string());
 
-        c1.push(accum_tuple);
+        c.push(accum_tuple);
     }      
     
     
-    return c1;
+    return c;
 }
 
 
-pub fn call_byzar(c1: Vec<(String, String)>)
+pub fn call_byzar(c: Vec<(String, String)>)
 {
     timer::wait(1);
 
     let v1: Vec<(String, String)> = Vec::new();
-    let v2: Vec<(String, String)> = Vec::new();
 
     //byzar()
     //byzar()
