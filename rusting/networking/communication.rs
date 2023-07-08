2b0c49b8e3c5e4a5d355491915c55f83d92c7add
zubayr1 <write2zubayr@gmail.com>
2023-07-08 21:11:05 +0200

    check stop

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -58,7 +58,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
             
             let mut count=1;
             for _ip in ip_address_clone.clone() 
-            {   println!("server    {:?}", _ip);
+            {   
                 count+=1;
                 let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
 
