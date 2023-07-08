14e246213182082728ce17d255e9bad19cb5f511
zubayr1 <write2zubayr@gmail.com>
2023-07-08 15:48:24 +0200

    recheck value

--- rusting/types/codeword.rs
+++ rusting/types/codeword.rs
@@ -7,37 +7,37 @@ mod merkle_tree;
 
 pub fn verify_codeword(value: String)
 {
-    let value_split: Vec<&str> = value.split(", ").collect();
+    // let value_split: Vec<&str> = value.split(", ").collect();
 
-    let mut leaf_values_to_prove: Vec<String> = Vec::new(); 
-    leaf_values_to_prove.push(value_split[1].to_string());
+    // let mut leaf_values_to_prove: Vec<String> = Vec::new(); 
+    // leaf_values_to_prove.push(value_split[1].to_string());
 
-    let indices_to_prove: Vec<usize> = vec![value_split[4].clone().parse().unwrap()];
+    // let indices_to_prove: Vec<usize> = vec![value_split[4].clone().parse().unwrap()];
 
     
-    let witness_str = value_split[2];
+    // let witness_str = value_split[2];
 
-    println!("{:?}", witness_str);
+    // println!("{:?}", witness_str);
 
-    let witness: Vec<u8> = witness_str
-    .strip_prefix('[')
-    .unwrap()
-    .strip_suffix(']')
-    .unwrap()
-    .split(", ")
-    .map(|s| s.parse().unwrap())
-    .collect();
+    // let witness: Vec<u8> = witness_str
+    // .strip_prefix('[')
+    // .unwrap()
+    // .strip_suffix(']')
+    // .unwrap()
+    // .split(", ")
+    // .map(|s| s.parse().unwrap())
+    // .collect();
 
-    let merkle_len = value_split[5].parse().unwrap();
+    // let merkle_len = value_split[5].parse().unwrap();
 
-    let accum_value = value_split[3];
+    // let accum_value = value_split[3];
 
-    let root = Vec::from_hex(accum_value.clone()).ok().unwrap();
-    let byte_root: [u8; 32] = root[..32].try_into().expect("Invalid length of byte vector");
-
-
-    let proof = merkle_tree::merkle_proof(witness.clone(), indices_to_prove.clone(), leaf_values_to_prove, byte_root, merkle_len);
+    // let root = Vec::from_hex(accum_value.clone()).ok().unwrap();
+    // let byte_root: [u8; 32] = root[..32].try_into().expect("Invalid length of byte vector");
 
 
-    println!("{:?}", proof);
+    // let proof = merkle_tree::merkle_proof(witness.clone(), indices_to_prove.clone(), leaf_values_to_prove, byte_root, merkle_len);
+
+
+    println!("{:?}", value);
 }
