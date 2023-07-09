31470ae1fe606c0b00122d8e4e4f18f82bfecff6
zubayr1 <write2zubayr@gmail.com>
2023-07-09 16:13:49 +0200

    check

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -357,12 +357,12 @@ value: String, merkle_len: usize,  witnesses_vec: Vec<Vec<u8>>, mode: String, me
     
             let codeword_vec = codeword.to_vec();
 
-    
-            let output = communication(committee_id.clone(), subset_vec.clone(), level, _index, args.clone(), port_count, 
-            medium.clone(), mode.clone(), initial_port, test_port, codeword_vec, committee_length).await;
+            println!("aaa");
+            // let output = communication(committee_id.clone(), subset_vec.clone(), level, _index, args.clone(), port_count, 
+            // medium.clone(), mode.clone(), initial_port, test_port, codeword_vec, committee_length).await;
             
-            // println!("{:?}", output);
-            codeword_output.push(output);
+            // // println!("{:?}", output);
+            // codeword_output.push(output);
         }
         else 
         {            
