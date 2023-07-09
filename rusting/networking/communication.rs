cfb307f51dd494c24cf5f7762265af9be2ffbf37
zubayr1 <write2zubayr@gmail.com>
2023-07-09 15:08:36 +0200

    check

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -65,7 +65,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
             {
                 for i in 0..length 
                 {
-                    println!("{:?}", count);
+                    println!("{:?},   {:?}", count, args[2].parse::<u32>().unwrap());
 
                     let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
 
