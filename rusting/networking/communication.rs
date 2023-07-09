ce0595e72bf827181335b92d3894c9374bccf252
zubayr1 <write2zubayr@gmail.com>
2023-07-10 00:03:43 +0200

    check communication

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -113,7 +113,6 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                                                             
                     
                 }
-                println!("{:?}", output);
             }
             
             
