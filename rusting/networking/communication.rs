1cf3fb2a26ca5354f2e45f472174ecf47e099d60
zubayr1 <write2zubayr@gmail.com>
2023-07-09 14:52:48 +0200

    check server

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -60,6 +60,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
 
             if types.contains("individual")
             {
+                for i in 0..2 {
                 let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
 
                 println!("server {:?}, {:?}, {:?}", (initial_port+port_count), (test_port+port_count + additional_port), additional_port);
@@ -76,6 +77,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                 {
                     output.push(_result);
                 }
+            }
 
             }
             else
