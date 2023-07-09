8f7eea9167838537849e074059206cf7b0a5dab1
zubayr1 <write2zubayr@gmail.com>
2023-07-09 23:54:16 +0200

    check communication

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -176,8 +176,8 @@ pub async fn reaction(output: Vec<Vec<String>>, medium: String, mode: String, co
                 
             }
             println!("{:?}", ip_address);
-            // let output = communication(committee_id.clone(), ip_address.clone(), level, _index, args.clone(), port_count, 
-            //                     medium.clone(), mode.clone(), initial_port, test_port, witness_to_deliver, "broadcast".to_string()).await;
+            let output = communication(committee_id.clone(), ip_address.clone(), level, _index, args.clone(), port_count, 
+                                medium.clone(), mode.clone(), initial_port, test_port, witness_to_deliver, "broadcast".to_string()).await;
             // println!("{:?}", output);
             // for output_string in output
             // {
