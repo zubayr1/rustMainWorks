fb21e3a0d0bae76c17aeaf989d679377f51acc3f
zubayr1 <write2zubayr@gmail.com>
2023-07-09 17:25:12 +0200

    check

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -72,10 +72,10 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
 
                     let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
 
-                    println!("server {:?}, {:?}, {:?}", (initial_port+port_count), (test_port+port_count + additional_port), additional_port);
+                    println!("server {:?}, {:?}, {:?}", (initial_port+port_count), (test_port+port_count ), additional_port);
 
                     let _result = newserver::handle_server( ip_address_clone.clone(), initial_port+port_count, 
-                    test_port+port_count+ additional_port );
+                    test_port+port_count );
 
                     println!("{:?}", _result);
                     println!("\n");
@@ -124,12 +124,12 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                 
                 let additional_port = (count + args[2].parse::<u32>().unwrap())*10;
 
-                println!("client {:?}, {:?}, {:?}", (initial_port+port_count), (test_port+port_count + additional_port), additional_port);
+                println!("client {:?}, {:?}, {:?}", (initial_port+port_count), (test_port+port_count), additional_port);
                 println!("client count: {:?},   {:?}", count, args[2].parse::<u32>().unwrap());
                 println!("{:?}", ip_address_clone);
 
                 let _result: Result<(), Box<dyn Error>> = newclient::match_tcp_client([ip_address_clone[0].to_string(), (initial_port+port_count).to_string()].join(":"),
-                [ip_address_clone[0].to_string(), (test_port+port_count + additional_port).to_string()].join(":"), 
+                [ip_address_clone[0].to_string(), (test_port+port_count ).to_string()].join(":"), 
                 committee_id.clone(), value.clone(), args.clone());
             }
             else 
