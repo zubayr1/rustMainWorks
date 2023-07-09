9e0bb5f15fc02acd89639ab7be04435df142a8e9
zubayr1 <write2zubayr@gmail.com>
2023-07-09 02:44:35 +0200

    check witness_to_deliver

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -158,7 +158,7 @@ pub async fn reaction(output: Vec<Vec<String>>, medium: String, mode: String, co
                     {
                         s_values.push(value_split.clone()[1].to_string());
 
-                        println!("{:?},   {:?}", value, value_split);
+                        
                         let witness_verify =  codeword::verify_codeword(value.clone());
 
                         if witness_verify==true
@@ -173,7 +173,7 @@ pub async fn reaction(output: Vec<Vec<String>>, medium: String, mode: String, co
 
                 
             }
-
+            println!("{:?}",  witness_to_deliver);
 
             let output = communication(committee_id.clone(), ip_address.clone(), level, _index, args.clone(), port_count, 
                                 medium.clone(), mode.clone(), initial_port, test_port, witness_to_deliver, committee_length).await;
