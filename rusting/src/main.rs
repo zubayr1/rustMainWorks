6279c9451d0b6393626b0c8a260acfd7c08750a3
zubayr1 <write2zubayr@gmail.com>
2023-07-09 16:14:18 +0200

    change port

--- rusting/src/main.rs
+++ rusting/src/main.rs
@@ -138,8 +138,8 @@ fn main()
 {
     println!("Starting");    
     
-    env::set_var("INITIAL_PORT", "7621");
-    env::set_var("TEST_PORT", "7721");
+    env::set_var("INITIAL_PORT", "7821");
+    env::set_var("TEST_PORT", "7921");
     
     let args: Vec<String> = env::args().collect(); // get user argument
     /*
