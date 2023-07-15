ac4dc71ab45e4780485137676774ec1be9879986
zubayr1 <write2zubayr@gmail.com>
2023-07-15 09:55:08 +0200

    check nodes_string

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -137,7 +137,8 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                         let reader = BufReader::new(file);
                     
                         let mut result: String =  "".to_string();
-                        for line_result in reader.lines() {
+                        for line_result in reader.lines() 
+                        {
                             let line = line_result.unwrap();
                             
                             if line.contains(socket_ip)
@@ -145,7 +146,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                                 let nodes_string: Vec<&str> = line.split(" ").collect();
 
                                 let level_usize: usize = level as usize;
-
+                                println!("{:?}", nodes_string);
                                 result = [_result.clone(), nodes_string.get(level_usize).unwrap().to_string()].join(", ");
                                 break;
                             }
