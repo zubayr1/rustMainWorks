c6a4662d619f629902091ddefb989a7144270a37
zubayr1 <write2zubayr@gmail.com>
2023-07-15 10:06:00 +0200

    send result

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -146,13 +146,13 @@ println!("{:?}",  level);
                                 let nodes_string: Vec<&str> = line.split(" ").collect();
 
                                 let level_usize: usize = level as usize;
-                                // println!("{:?}, {:?}", nodes_string, level_usize);
-                                // result = [_result.clone(), nodes_string.get(level_usize).unwrap().to_string()].join(", ");
+                                println!("{:?}, {:?}", nodes_string, level_usize);
+                                result = [_result.clone(), nodes_string.get(level_usize).unwrap().to_string()].join(", ");
                                 break;
                             }
                         }
 
-                        output.push(_result);
+                        output.push(result);
                     }
 
 
