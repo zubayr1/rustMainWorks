a8c8b5b91f70f181d46ef1e1bc292960b117b0d7
zubayr1 <write2zubayr@gmail.com>
2023-07-27 01:31:27 +0200

    loopback port

--- rusting/networking/newserver.rs
+++ rusting/networking/newserver.rs
@@ -9,7 +9,7 @@ use tokio::fs::OpenOptions;
 #[tokio::main]
 pub async fn handle_server( _ip_address: Vec<&str>, port: u32, testport: u32) -> String{
    // loop{
-    let listener = TcpListener::bind(["0.0.0.0".to_string(), "0".to_string()].join(":")).await.unwrap(); // open connection
+    let listener = TcpListener::bind(["0.0.0.0".to_string(), port.to_string()].join(":")).await.unwrap(); // open connection
     
     let test_listener = TcpListener::bind(["0.0.0.0".to_string(), testport.to_string()].join(":")).await.unwrap();
 
