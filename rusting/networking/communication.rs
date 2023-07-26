0215e68e76afc860d033ed3214854c5f36c2661f
zubayr1 <write2zubayr@gmail.com>
2023-07-26 22:15:31 +0200

    add ports

--- rusting/networking/communication.rs
+++ rusting/networking/communication.rs
@@ -89,7 +89,7 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
             if types.contains("individual")
             {
                             
-                let additional_port = (client_count)*30;
+                let additional_port = (client_count)*10;
 
                 let _result = newserver::handle_server( ip_address_clone.clone(), initial_port+port_count, 
                 test_port+port_count+ additional_port );
@@ -109,11 +109,11 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                 for _ip in ip_address_clone.clone() 
                 {   
                     count+=1;
-                    let mut additional_port = (count + args[2].parse::<u32>().unwrap())*15;
+                    let mut additional_port = (count + args[2].parse::<u32>().unwrap())*25;
 
                     if mode=="codeword"
                     {
-                        additional_port = (count + args[2].parse::<u32>().unwrap())*100;
+                        additional_port = (count + args[2].parse::<u32>().unwrap())*50;
 
                         let _result = newserver::handle_server( ip_address_clone.clone(), initial_port+port_count, 
                         test_port+port_count + additional_port);
@@ -153,7 +153,11 @@ pub async fn prod_communication(committee_id: u32, ip_address: Vec<&str>, level:
                         output.push(result);
                     }
 
-                                       
+
+                    
+
+                    
+                                                            
                     
                 }
             }
