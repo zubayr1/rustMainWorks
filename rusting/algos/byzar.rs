51cf70eead9820d9a8ca83e0ddcd99aed500202f
zubayr1 <write2zubayr@gmail.com>
2023-07-27 01:08:16 +0200

    check

--- rusting/algos/byzar.rs
+++ rusting/algos/byzar.rs
@@ -9,7 +9,7 @@ pub async fn byzar(committee_id: u32, ip_address: &Vec<&str>, level: u32, port_c
     let (V, _g) = gba::gba(committee_id, ip_address.to_vec(), level, port_count, _index, args.clone(),
     V.clone(), medium, mode.clone(), types, committee_length).await;
 
-    //println!("{:?}, {:?}", V, _g);
+    println!("{:?}, {:?}", V, _g);
 
     return V;
     
