974301e4fd496b10cce1466287f635df7e418f08
zubayr1 <write2zubayr@gmail.com>
2023-07-28 13:31:31 +0200

    check comm complexity

--- rusting/networking/newserver.rs
+++ rusting/networking/newserver.rs
@@ -71,6 +71,8 @@ pub async fn handle_server( _ip_address: Vec<&str>, port: u32, testport: u32) ->
 
 
     line = [decoded_string, socket_ip[0].to_string()].join("/");    
+
+    println!("{}", line);
     return line;
     
 //}
