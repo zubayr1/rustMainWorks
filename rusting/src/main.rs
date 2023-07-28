73e173805c7877019845fec1e2b69b2824a66a03
zubayr1 <write2zubayr@gmail.com>
2023-07-28 12:30:06 +0200

    update nodes

--- rusting/src/main.rs
+++ rusting/src/main.rs
@@ -140,8 +140,8 @@ fn main()
 {
     println!("Starting");    
     
-    env::set_var("INITIAL_PORT", "8121");
-    env::set_var("TEST_PORT", "8221");
+    env::set_var("INITIAL_PORT", "7121");
+    env::set_var("TEST_PORT", "7221");
     
     let args: Vec<String> = env::args().collect(); // get user argument
     /*
