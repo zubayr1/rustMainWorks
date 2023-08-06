1a547f7a18a863a41bf43d281cb82c9ccf8477fa
zubayr1 <write2zubayr@gmail.com>
2023-08-06 16:23:10 +0200

    check

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -116,7 +116,7 @@ async fn port_testing(server_stream_vec_rc: Vec<Rc<TcpStream>>, client_stream_ve
     .into_iter()
         .filter_map(|rc| Rc::try_unwrap(rc).ok())
         .collect();
-
+    println!("{:?}",client_stream_vec);
     // Split the server_stream_vec into individual streams
     let mut server_streams = Vec::new();
     while let Some(stream) = server_stream_vec.pop() {
@@ -273,7 +273,7 @@ pub async fn initiate(filtered_committee: HashMap<u32, String>, args: Vec<String
                         },
                     }
                 }
-                println!("{:?}", server_stream_vec_final_rc);
+                // println!("{:?}", server_stream_vec_final_rc);
                 let server_stream_vec_final: Vec<TcpStream> = server_stream_vec_final_rc
                 .into_iter()
                     .filter_map(|rc| Rc::try_unwrap(rc).ok())
