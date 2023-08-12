1d594a825289363726dfce6800412f0727a3a2d4
zubayr1 <write2zubayr@gmail.com>
2023-08-12 13:29:57 +0200

    check

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -188,9 +188,9 @@ pub async fn initiate(filtered_committee: HashMap<u32, String>, args: Vec<String
         sockets.push([ip, "7000".to_string()].join(":").parse::<SocketAddr>().unwrap());
     }  
     
-    tokio::spawn(async move {
+    //tokio::spawn(async move {
         node::Node::new(1, sockets).await;
-    });
+    //});
     
 
     for _index in 1..(args[7].parse::<u32>().unwrap()+1) // iterate for all epoch
