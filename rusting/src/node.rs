d4fafa0114533f5122f60d62441deb6bcd67dd61
zubayr1 <write2zubayr@gmail.com>
2023-08-12 16:02:52 +0200

    check

--- rusting/src/node.rs
+++ rusting/src/node.rs
@@ -3,36 +3,34 @@ use std::net::SocketAddr;
 use tokio::sync::mpsc::channel;
 use tokio::time::{sleep, Duration};
 
-use crate::{
-    core::Core,
-    network::*,
-};
+use crate::{core::Core, network::*};
 
 pub struct Node;
 
 impl Node {
-    pub async fn new(id: usize, nodes: Vec<SocketAddr>) 
-    {        
-         // Create channels for the networking.
-         let (tx_rec, rx_rec) = channel(10_000);
-         let (tx_send, rx_send) = channel(10_000);
- 
-         // Create a network receiver and sender.
-         let network_receiver = NetworkReceiver::new(tx_rec);
-         let mut network_sender = NetworkSender::new(rx_send);
- 
-         tokio::spawn(async move {
-             network_receiver.run().await;
-         });
-         tokio::spawn(async move {
-             network_sender.run().await;
-         });
- 
-         sleep(Duration::from_millis(50)).await;
-         
-         println!("{:?}", nodes[id]);
-         Core::spawn(id, nodes[id], nodes, tx_send, rx_rec);
-    }
+    pub async fn new(id: usize, nodes: Vec<SocketAddr>) {
+        // Create channels for the networking.
+        let (tx_rec, rx_rec) = channel(10_000);
+        let (tx_send, rx_send) = channel(10_000);
 
-    
+        // Create a network receiver and sender.
+        let network_receiver = NetworkReceiver::new(tx_rec);
+        let mut network_sender = NetworkSender::new(rx_send);
+
+        tokio::spawn(async move {
+            network_receiver.run().await;
+        });
+        tokio::spawn(async move {
+            network_sender.run().await;
+        });
+
+        sleep(Duration::from_millis(50)).await;
+
+        Core::spawn(id, nodes[id], nodes, tx_send, rx_rec);
+
+        println!("check");
+
+        sleep(Duration::from_millis(1000)).await;
+        println!("checked");
+    }
 }
