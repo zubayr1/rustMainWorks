145a4a40348028ed1c40839d1f4210dc0237e401
zubayr1 <write2zubayr@gmail.com>
2023-08-18 16:42:01 +0200

    check output in codeword_output

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -268,7 +268,10 @@ pub async fn reactor<'a>(
         codeword_output = codeword_reactor(committee_id, ip_address, level, _index, args.clone(), port_count, 
             value, merkle_len, codeword_vec, witnesses_vec, mode.clone(), medium.clone(), committee_length, initial_port, test_port).await;
 
-        println!("{:?}", codeword_output);
+        for output in codeword_output
+        {
+            println!("{:?}", output);
+        }
         // let _codeword_reaction_check = reaction(codeword_output, medium, mode, committee_length,            
         //     committee_id, ip_address, level, _index,  args, port_count, 
         //     initial_port, test_port
