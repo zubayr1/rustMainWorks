1995df5041129fb35e5a65a394dbcda3c32a5ced
zubayr1 <write2zubayr@gmail.com>
2023-08-19 20:55:49 +0200

    original_data, leaves, num_nodes

--- rusting/algos/pvss_agreement.rs
+++ rusting/algos/pvss_agreement.rs
@@ -77,8 +77,7 @@ pub fn encoder(pvss_data: &[u8], mut committee_size: usize, medium: String) -> V
         .map(|inner_vec| format!("{:?}", inner_vec))
         .collect();
 
-    println!("{:?}", original_data);
-    println!("{:?}", leaves);
+    println!("{:?}, {:?}, {:?}", original_data, leaves, num_nodes);
     return leaves;
 
 
