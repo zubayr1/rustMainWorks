978f4cf0b5371f7278cbf71c6b75c589c387d99d
zubayr1 <write2zubayr@gmail.com>
2023-08-19 21:23:31 +0200

    (codeword_vec, witnesses_vec, merkle_len, qual)

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -227,10 +227,10 @@ pub async fn reactor<'a>(
         let codeword_output = codeword_reactor(committee_id, ip_address, level, _index, args.clone(), port_count, 
             value, merkle_len, codeword_vec, witnesses_vec, mode.clone(), medium.clone(), initial_port, test_port).await;
 
-        for output in codeword_output.clone()
-        {
-            println!("{:?}", output);
-        }
+        // for output in codeword_output.clone()
+        // {
+        //     println!("{:?}", output);
+        // }
         let _codeword_reaction_check = reaction(codeword_output, medium, mode, committee_length,            
             committee_id, ip_address, level, _index,  args, port_count, 
             initial_port, test_port
@@ -245,7 +245,7 @@ pub async fn reactor<'a>(
             pvss_data.clone(), committee_id, &ip_address, level, _index, args.clone(), port_count, 
             value.clone(), mode, medium.clone(), committee_length, initial_port, test_port, qual).await;
 
-
+            println!("{:?},    {:?},     {:?},   {:?}", (codeword_vec, witnesses_vec, merkle_len, qual));
         return reactor(pvss_data, committee_id, ip_address, level, _index, args, port_count, value, 
             merkle_len, codeword_vec, witnesses_vec, "codeword".to_string(), medium, committee_length, qual).await;
     }
