4c8e206be83fbf5fe02b50b193925551f20c0a0b
zubayr1 <write2zubayr@gmail.com>
2023-08-19 21:14:53 +0200

    output

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -227,7 +227,10 @@ pub async fn reactor<'a>(
         let codeword_output = codeword_reactor(committee_id, ip_address, level, _index, args.clone(), port_count, 
             value, merkle_len, codeword_vec, witnesses_vec, mode.clone(), medium.clone(), initial_port, test_port).await;
 
-        println!("{:?}", codeword_output);
+        for output in codeword_output.clone()
+        {
+            println!("{:?}", output);
+        }
         let _codeword_reaction_check = reaction(codeword_output, medium, mode, committee_length,            
             committee_id, ip_address, level, _index,  args, port_count, 
             initial_port, test_port
