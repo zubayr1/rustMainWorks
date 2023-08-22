642919a95e3919361eabf9908cbfce9904d6e075
zubayr1 <write2zubayr@gmail.com>
2023-08-23 00:49:11 +0200

    check 2byzar

--- rusting/algos/byzar.rs
+++ rusting/algos/byzar.rs
@@ -43,6 +43,9 @@ pub async fn twobyzar<'a>(
     
         let new_committee_length = committee_length/2;
 
+        println!("first half {:?}", first_half);
+        println!("second half {:?}", second_half);
+
         twobyzar( 
             committee_id, &first_half.to_vec(), level, port_count, _index, 
             args.clone(), V.clone(), mode.clone(), types.clone(), new_committee_length).await
@@ -57,7 +60,7 @@ pub async fn twobyzar<'a>(
     }
     else 
     {   
-        println!("{:?}", ip_address_vec);
+        
         // call some bizantine protocol
         return true;
     }
