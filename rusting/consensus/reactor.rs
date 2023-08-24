66692f92d868eeac9610cd3cf4094617fbb56c60
zubayr1 <write2zubayr@gmail.com>
2023-08-24 20:55:16 +0200

    acc_value_zl

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -410,7 +410,7 @@ pub async fn accum_reactor(
     let qual_clone = qual.clone();
     
     for val in qual
-    {   println!("{:?}", val);
+    {   
         if val==1 && v1==acc_value_zl
         {
             (codeword_vec, witnesses_vec, merkle_len) = deliver::deliver_encode(pvss_data.as_bytes(), v1.clone(), committee_length.clone());
@@ -418,7 +418,7 @@ pub async fn accum_reactor(
         }
 
         if val==2 && v2==acc_value_zl
-        {
+        {   println!("{:?}",acc_value_zl);
             (codeword_vec, witnesses_vec, merkle_len) = deliver::deliver_encode(pvss_data.as_bytes(), v2.clone(), committee_length.clone());
 
         }
