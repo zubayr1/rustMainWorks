297c221987c5bd0f5dca3c0f4b6fff87fd5643c8
zubayr1 <write2zubayr@gmail.com>
2023-09-07 00:34:54 +0200

    add sleep

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -838,7 +838,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                         }
 
-                                                
+                        sleep(Duration::from_millis(10)).await;                 
 
                         vote1_value = Vec::new();       
 
