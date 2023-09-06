013ff3321db9979bc897e9e81766591584ca5c03
zubayr1 <write2zubayr@gmail.com>
2023-09-06 21:37:51 +0200

    ForwardMessage

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -732,7 +732,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     //     forward_value.push(value);
                     // }
 
-                                        
+                    forward_value.push(value);                    
 
                     if forward_value.len()==ip_address.clone().len() 
                     { 
