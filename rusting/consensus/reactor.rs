a00d725039635f1d122579783577f27240f3cbaf
zubayr1 <write2zubayr@gmail.com>
2023-09-06 20:09:52 +0200

    increase time

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -964,7 +964,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                         }
 
                         // do here
-                        sleep(Duration::from_millis(150)).await;
+                        sleep(Duration::from_millis(450)).await;
 
                         if V1!="bot" && V1!=""
                         {        
