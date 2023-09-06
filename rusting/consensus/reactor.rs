e4842a6179cecc4ef12f42dcd0015d70a6845def
zubayr1 <write2zubayr@gmail.com>
2023-09-07 01:15:06 +0200

    increase sleep

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1025,7 +1025,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                             }
                             else 
                             {
-                               sleep(Duration::from_millis(200)).await;
+                               sleep(Duration::from_millis(400)).await;
                                return;
                             }
 
