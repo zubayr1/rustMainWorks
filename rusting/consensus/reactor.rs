5a93dc8c7f2d94c7cd19f0a136b5d7da0b9c7254
zubayr1 <write2zubayr@gmail.com>
2023-09-07 01:19:29 +0200

    decrease sleeps

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1024,7 +1024,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                             }
                             else 
                             {
-                               sleep(Duration::from_millis(200)).await;
+                               sleep(Duration::from_millis(20)).await;
                                return;
                             }
 
