44bd85a2a3793534c3df7d2b053b130033af7bd6
zubayr1 <write2zubayr@gmail.com>
2023-09-11 16:48:19 +0200

    add time

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1281,7 +1281,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                     let value = format!("{} {}", propose.value,  message.sender);
                    
-                    sleep(Duration::from_millis(0)).await;
+                    sleep(Duration::from_millis(10)).await;
 
                     propose_value.push(value); 
 
