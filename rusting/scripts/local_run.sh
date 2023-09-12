d75d1021823524ff438b822644f41fce50cc8d1e
zubayr1 <write2zubayr@gmail.com>
2023-09-12 23:55:07 +0200

    edit local run

--- rusting/scripts/local_run.sh
+++ rusting/scripts/local_run.sh
@@ -7,14 +7,7 @@ while IFS= read -r line; do
     IPS+=($line)
 done < "$FILE"
 
-# Function to introduce a 10ms delay
-delay() {
-    local n=$1
-    while [ "$n" -gt 0 ]; do
-        sleep 0.01  # Sleep for 10 milliseconds
-        n=$((n-1))
-    done
-}
+
 
 # Connect to every IP and run run.sh on instance
 for _ip in "${IPS[@]}"; do
@@ -23,5 +16,4 @@ for _ip in "${IPS[@]}"; do
     ip=${tmp[1]}
     echo "$ip"
     ssh -i "randpiper.pem" ubuntu@"$ip" "bash run.sh" &
-    delay 1  # Introduce a 10ms delay before the next SSH connection
 done
