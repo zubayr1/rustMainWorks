0521800960df66b3217bbf4b399567105b3b5f63
zubayr1 <write2zubayr@gmail.com>
2023-09-12 23:17:46 +0200

    loopback reactor

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -781,7 +781,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
     let mut retrieved_hashmap_codeword: HashMap<usize, HashMap<SocketAddr, String>> = HashMap::new();
     let mut retrieved_hashmap_committee: HashMap<usize, HashMap<SocketAddr, String>> = HashMap::new();
 
-    let mut retrieved_hashmap: HashMap<usize, HashMap<SocketAddr, String>>;
+    let mut retrieved_hashmap: HashMap<usize, HashMap<SocketAddr, String>> = HashMap::new();
 
 
     if ip_address.len()==1
@@ -1015,7 +1015,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                 ConsensusMessage::CodewordRetrieveMessage(retrieve) =>
                 {   
-                    // println!("received cordwordretrieve, {:?}, {}", message.sender, message.level);
+                    println!("received cordwordretrieve, {:?}, {}", message.sender, message.level);
                     // Handle Retrieve message
 
                     let mut total_length_codeword = 0;
@@ -1063,7 +1063,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     else 
                     {
                         total_length = total_length_committee;
-
+                        println!("                       {}", total_length_committee);
                         retrieved_hashmap = retrieved_hashmap_committee.clone();
                     }
                     
@@ -1109,7 +1109,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     if flag == 1
                     {   
                         // sleep(Duration::from_millis(20)).await;
-
+                        println!("    {}, {}", 2*ip_address.clone().len(),total_length);
                         if total_length == 2*ip_address.clone().len() 
                         {   
                             flag = 0;
@@ -1127,6 +1127,8 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                             
                             let pvss_vec = codeword_retrieve(retrieved_hashmap.clone(), 
                                 ip_address.clone().len());
+
+                            retrieved_hashmap = HashMap::new();
 
                             
 
