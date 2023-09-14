f7e38964370c328bb56df3235dad905acd49aea1
zubayr1 <write2zubayr@gmail.com>
2023-09-14 13:23:08 +0200

    flag

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1105,7 +1105,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     }
                     else 
                     { 
-                        println!("   {}, {},  {}", communication_type, message.sender, message.level);
+                        println!("   {}, {},  {}, f{}", communication_type, message.sender, message.level, flag);
 
                         let part = retrieve.part.clone();
 
