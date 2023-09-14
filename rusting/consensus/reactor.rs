1a2d5833fbb403cc02609a685d72e8a252fdd8fb
zubayr1 <write2zubayr@gmail.com>
2023-09-14 18:41:34 +0200

    add push

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -378,7 +378,7 @@ async fn codeword_helper(tx_sender: Sender<NetworkMessage>, communication_type: 
                 
                 let _ = tx_sender.send(codewordretrieve_network_message.clone()).await;
 
-                // check_first_codeword_list.push(value.clone());
+                check_first_codeword_list.push(value.clone());
             }
         }
     }
@@ -429,7 +429,7 @@ async fn codeword_helper(tx_sender: Sender<NetworkMessage>, communication_type: 
 
                 let _ = tx_sender.send(codewordretrieve_network_message.clone()).await;
 
-                // check_first_committee_list.push(value.clone());
+                check_first_committee_list.push(value.clone());
             }
         }
     }
