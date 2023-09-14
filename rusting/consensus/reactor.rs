9c7a9b5db5d3d986329989ba0ce23670eef2fd60
zubayr1 <write2zubayr@gmail.com>
2023-09-14 19:14:25 +0200

    LEVEL 4 accum val

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1177,8 +1177,11 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                                 ip_address = ip_addresses_comb.split(" ").collect();
                                         
                                 (acc_value_zl, state) = reactor_init(pvss_data.clone(), ip_address.clone());
-                            
                                 
+                                if level==4
+                                {
+                                    println!("LEVEL 4 accum val {}", acc_value_zl);
+                                }
                                 let accum_network_message = accum_init(acc_value_zl.clone(), ip_address.clone(), 
                                     args.clone(), level.clone());
                             
