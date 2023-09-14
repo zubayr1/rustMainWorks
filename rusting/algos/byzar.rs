93f8c043535a33b5ec2cc9ee34638b127e4be307
zubayr1 <write2zubayr@gmail.com>
2023-09-14 20:57:44 +0200

    V VALUE

--- rusting/algos/byzar.rs
+++ rusting/algos/byzar.rs
@@ -13,7 +13,7 @@ pub async fn BA_setup(
     tx_sender: Sender<NetworkMessage>, ip_address: Vec<&str>, 
     args: Vec<String>, V: String) 
 {
-
+    println!("V VALUE {}, {}", V, ip_address.len());
     gba::gba_setup(tx_sender, ip_address, args, V).await;
 }   
 
