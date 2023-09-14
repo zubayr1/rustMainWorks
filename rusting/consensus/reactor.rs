1b619112b36751e741f3e7e2afb045abb76b73dd
zubayr1 <write2zubayr@gmail.com>
2023-09-14 20:38:45 +0200

    forward

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -934,7 +934,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                             let first_part = parts[0];
                             first_part == first_part_to_compare
                         });
-
+                        println!("forward {}", message.level);
                         forward_value = Vec::new(); 
 
                         if all_parts_match && forward_check{
