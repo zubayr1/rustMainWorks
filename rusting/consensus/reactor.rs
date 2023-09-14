9db64cb5f87e283959db3c518f3e22d691f041b6
zubayr1 <write2zubayr@gmail.com>
2023-09-14 21:59:08 +0200

    propose_value

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1432,7 +1432,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                     
                     if propose_value.len() == ip_address.clone().len()/2
-                    {    
+                    {    println!("propose_value len {}", ip_address.clone().len());
                         if g==0
                         {
                             let (most_frequent, is_majority) = find_most_frequent_propose_value(
