67b1a065fc2d971beaf08937fa52cd3012af7c54
zubayr1 <write2zubayr@gmail.com>
2023-09-16 01:28:55 +0200

    echo check

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -888,7 +888,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     let (count, pi): (usize, Vec<String>);
 
                     if echo_value.len()==2_usize.pow(level as u32)    
-                    {   
+                    {   println!("uessss {}", message.level);
                         let V = format!("{}-{}", V1.clone(), V2.clone());
                         (count, pi) = gba::check_echo_major_v(echo_value.clone(), V.clone());
 
