e7be6895c0cf51697edf536fa814c4bb0e9976a9
zubayr1 <write2zubayr@gmail.com>
2023-09-16 18:25:28 +0200

    accum check

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1273,7 +1273,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                             .insert(message.sender, value);
                         
                     }
-                    println!("{}, {}, {}", message.level, level, accum_value.len());
+                    println!("ACCUM {}, {}, {}, {},    {}", message.level, level, state.get_level(), ip_address.clone().len(),  accum_value.len());
                     if accum_value.len()==ip_address.clone().len()
                     {   
                         split_vec_recursively(&ip_address, &mut ip_address_left, &mut ip_address_right);
