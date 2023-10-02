640acebc5aca195fa8e172839303a1a995a6c388
zubayr1 <write2zubayr@gmail.com>
2023-10-02 15:27:48 +0200

    codeword_helper loopback

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -358,9 +358,9 @@ async fn codeword_helper(tx_sender: Sender<NetworkMessage>, communication_type: 
     args: Vec<String>, check_first_codeword_list: Vec<String>, check_first_committee_list: Vec<String>, level: usize, pvss_data: Vec<u8>)
     -> (String, Vec<String>, Vec<String>)
 {
-    let mut data = String::from_utf8_lossy(&pvss_data).to_string();
+    // let mut data = String::from_utf8_lossy(&pvss_data).to_string();
 
-
+    let mut data = "pvss".to_string();
     if ip_address.len()==2
     {
         let bytes = codewords.trim_matches('[').trim_matches(']').split("; ");
