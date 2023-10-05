125440083933125558dcb46217faf9599ac5b08f
zubayr1 <write2zubayr@gmail.com>
2023-10-06 01:46:41 +0200

    cgeck aggregate share

--- rusting/crypto/aggregrate.rs
+++ rusting/crypto/aggregrate.rs
@@ -1,28 +1,31 @@
 extern crate optrand_pvss;
-use ark_bls12_381::Bls12_381;
-use ark_ec::PairingEngine;
 
-use optrand_pvss::signature::schnorr::SchnorrSignature;
-
-use optrand_pvss::modified_scrape::participant::Participant;
 use ark_serialize::{CanonicalSerialize, CanonicalDeserialize};
-use optrand_pvss::modified_scrape::share::PVSSAggregatedShare;
+use optrand_pvss::modified_scrape::share::*;
 
 
 pub fn aggregrate_intermediate(share1: Vec<u8>, share2: Vec<u8>, num_participants: usize)
 {
-    let deserialized_share1: Participant<Bls12_381, SchnorrSignature<<Bls12_381 as PairingEngine>::G1Affine>> = 
-        Participant::deserialize(&share1[..]).unwrap();
+    let deserialized_share1: optrand_pvss::modified_scrape::share::PVSSAggregatedShare<ark_ec::bls12::Bls12<ark_bls12_381::Parameters>> = 
+        PVSSAggregatedShare::deserialize(&share1[..]).unwrap();
 
-    let deserialized_share2: Participant<Bls12_381, SchnorrSignature<<Bls12_381 as PairingEngine>::G1Affine>> = 
-        Participant::deserialize(&share2[..]).unwrap();
+    let deserialized_share2: optrand_pvss::modified_scrape::share::PVSSAggregatedShare<ark_ec::bls12::Bls12<ark_bls12_381::Parameters>> = 
+    PVSSAggregatedShare::deserialize(&share2[..]).unwrap();
+
+    let degree =  num_participants/2 - 1;
+
+    // let share1: PVSSAggregatedShare<_> = PVSSAggregatedShare::empty(degree, num_participants);
+    // let share2: PVSSAggregatedShare<_> = PVSSAggregatedShare::empty(degree, num_participants);
 
     
     match deserialized_share1.aggregate(&deserialized_share2) {
         Ok(aggregated_share) => {
             // The result is an aggregated PVSS share
             // You can use the aggregated_share for further operations
-            println!("Aggregated share: {:?}", aggregated_share);
+            let mut serialized_data = Vec::new();
+            aggregated_share.serialize(&mut serialized_data).unwrap();
+
+            println!("Aggregated share: {:?}", serialized_data);
         }
         Err(err) => {
             // Handle the error if the aggregation fails
