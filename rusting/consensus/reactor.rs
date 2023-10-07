bf81045f3c2aa27a1eb657e31e720cb4a8f23ad4
zubayr1 <write2zubayr@gmail.com>
2023-10-07 18:02:38 +0200

    pvss_data.len()

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1020,6 +1020,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                         pvss_data = serialized_data;
                         
+                        println!("AT LEVEL 0: {:?}", pvss_data.len());
 
                         (_, ip_addresses_comb) = sorted[level];
 
