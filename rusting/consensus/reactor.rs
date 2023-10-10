4028cea7938c267c8c62dd8a7adb8dafea7d879b
zubayr1 <write2zubayr@gmail.com>
2023-10-10 18:22:37 +0200

    check at level 0

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1113,7 +1113,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
 
                         pvss_data = serialized_data;
                         
-                        println!("AT LEVEL 0: {:?}", pvss_data.len());
+                        println!("AT LEVEL 0: {:?}", pvss_data);
 
                         (_, ip_addresses_comb) = sorted[level];
 
