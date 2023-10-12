86a2db0ab844d1844a8daea8589df6a45bcfd8be
zubayr1 <write2zubayr@gmail.com>
2023-10-12 19:06:15 +0200

    level

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1182,7 +1182,7 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                     let value = format!("{} {}", echo.value, message.sender);
                     
                     if message.level == level
-                    {
+                    {   println!("{}", level);
                         echo_value.push(value);
                     }   
 
