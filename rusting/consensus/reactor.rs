c77a06efa2297574bec96fd2bcdb3f4eeaf9eed8
zubayr1 <write2zubayr@gmail.com>
2023-10-12 22:03:02 +0200

    size

--- rusting/consensus/reactor.rs
+++ rusting/consensus/reactor.rs
@@ -1795,8 +1795,19 @@ pub async fn reactor(tx_sender: Sender<NetworkMessage>, mut rx: Receiver<Network
                         
                     }
 
+                    let mut size = 0;
+
+                    if propose_reached==false
+                    {
+                        size = 2_usize.pow(level as u32)/2 ;
+                    }
+                    else 
+                    {
+                        size = 2_usize.pow(level as u32)/4 ;
+                    }
+
                     
-                    if propose_value.len() == 2_usize.pow(level as u32)/2 && message.level == level
+                    if propose_value.len() == size && message.level == level
                     {    println!("PROPOSE {:?}", propose_value);
                         if g==0
                         {
